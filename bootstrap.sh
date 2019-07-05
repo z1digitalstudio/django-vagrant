92545bbf60631c1ced040330df12dda404f6fb6c
Javier Borrego <javierborregocejudo@gmail.com>
2019-07-05 17:02:40 +0200

    Fix Bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get -y upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
 
-apt-get -y install python-dev
-apt-get -y install libpq-dev
-apt-get -y install libffi-dev libssl-dev libxml2-dev libxslt1-dev
-apt-get -y install libxmlsec1 libxmlsec1-dev swig
-apt-get -y install libjpeg-dev libpng12-dev
-apt-get -y install python-lxml python-cffi libcairo2 libpango1.0-0 libgdk-pixbuf2.0-0 shared-mime-info
+sudo apt-get -y install python-dev
+sudo apt-get -y install libpq-dev
+sudo apt-get -y install libffi-dev libssl-dev libxml2-dev libxslt1-dev
+sudo apt-get -y install libxmlsec1 libxmlsec1-dev swig
+sudo apt-get -y install libjpeg-dev libpng12-dev
+sudo apt-get -y install python-lxml python-cffi libcairo2 libpango1.0-0 libgdk-pixbuf2.0-0 shared-mime-info
 
-apt-get -y install redis-server
+sudo apt-get -y install redis-server
 
-apt-get -y install git
+sudo apt-get -y install git
 
 # YOU CAN UNCOMMENT NEXT LINES TO USE NODEJS
 # curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
@@ -21,19 +21,19 @@ apt-get -y install git
 # sudo ln -s /usr/bin/nodejs /usr/sbin/node
 # apt-get -y install npm
 
-apt-get -y install python3-pip
+sudo apt-get -y install python3-pip
 
 # Yarn
 # curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 # echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 # Postgresql 9.4
-echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-apt-get update
-apt-get -y install postgresql-9.4 postgresql-server-dev-9.4 pgadmin3 python-psycopg2
 
-apt-get -y install yarn
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+sudo apt install wget ca-certificates
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add
+sudo apt update
+sudo apt-get -y install postgresql-9.4 postgresql-server-dev-9.4 pgadmin3 python-psycopg2
 
 echo "local all postgres peer" > /etc/postgresql/9.4/main/pg_hba.conf
 echo "local django_db django md5" >> /etc/postgresql/9.4/main/pg_hba.conf
@@ -82,3 +82,8 @@ sed -i.bak "s/^bind 127.0.0.1$/# bind 127.0.0.1/" /etc/redis/redis.conf
 sed -i.bak "s/^port 6379$/# port 6379/" /etc/redis/redis.conf
 
 service redis-server restart
+
+pgadmin3 : Depends: libgcrypt11 (>= 1.5.1) but it is not installable
+    default:             Depends: libwxbase3.0-0 (>= 3.0.0) but it is not installable
+    default:             Depends: libwxgtk3.0-0 (>= 3.0.0) but it is not installable
+    default:             Recommends: pgagent but it is not going to be installed
